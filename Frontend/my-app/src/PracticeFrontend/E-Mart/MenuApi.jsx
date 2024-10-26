4010ab469f704ee3672226c71566185362e067e1
Ankit <ankitkumarmaurya98@gmail.com>
2024-10-26 23:09:30 +0530

    all unwanted file delet

--- Frontend/my-app/src/PracticeFrontend/E-Mart/MenuApi.jsx
+++
@@ -1,9 +0,0 @@
-import axios from "./axios";
-
-const response = await axios.get('/GetProductDetails');
-
-const Menu = response.data.result;
-
-console.log('Menu',Menu)
-
-export default Menu;
--- Frontend/my-app/src/axios.jsx
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-// we need to pass the baseURL as an object
-const api = axios.create({
-  baseURL: "http://localhost:8010",
-  withCredentials: true
-});
-
-export default api;
